8632d49c0c7e7c9d427d48818dd2bcaf4664671c
William Welling <wwelling@library.tamu.edu>
2019-10-11 14:08:43 -0500

    cleaner streaming reference links

--- src/main/java/org/folio/rest/controller/StreamingReferenceLinkController.java
+++ src/main/java/org/folio/rest/controller/StreamingReferenceLinkController.java
@@ -1,58 +1,35 @@
 package org.folio.rest.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.servlet.http.HttpServletResponse;
 
 import org.folio.rest.model.ReferenceLink;
-import org.folio.rest.model.repo.ReferenceLinkRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/streaming/referenceLinks")
 public class StreamingReferenceLinkController {
 
-  private static final Logger logger = LoggerFactory.getLogger(ReferenceLinkController.class);
-
-  @Autowired
-  private ReferenceLinkRepo referenceLinkRepo;
-
-  @Autowired
-  private ObjectMapper objectMapper;
+  private static final Logger logger = LoggerFactory.getLogger(StreamingReferenceLinkController.class);
 
   @PersistenceContext
   private EntityManager entityManager;
 
-  @Transactional(readOnly = true)
-  @GetMapping(value = "/type/{id}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-  public void findAllByTypeId(HttpServletResponse response, @PathVariable String id) throws IOException {
-    logger.info("Streaming all ExternalReference by type id: {}", id);
-    try (Stream<ReferenceLink> links = referenceLinkRepo.streamAllByTypeId(id)) {
-      PrintWriter out = response.getWriter();
-      links.forEach(link -> {
-        try {
-          out.write(objectMapper.writeValueAsString(link));
-          out.write("\n");
-        } catch (JsonProcessingException e) {
-          e.printStackTrace();
-        }
-      });
-    }
+  @GetMapping(value = "/type/{typeId}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+  public Flux<ReferenceLink> streamAllByTypeId(@PathVariable String typeId) throws IOException {
+    logger.info("Streaming all ExternalReference by type id: {}", typeId);
+    String query = String.format("SELECT r FROM ReferenceLink r WHERE r.type.id = '%s'", typeId);
+    return Flux.fromStream(entityManager.createQuery(query, ReferenceLink.class).getResultStream());
   }
 
 }
--- src/main/java/org/folio/rest/model/repo/ReferenceLinkRepo.java
+++ src/main/java/org/folio/rest/model/repo/ReferenceLinkRepo.java
@@ -1,11 +1,9 @@
 package org.folio.rest.model.repo;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.folio.rest.model.ReferenceLink;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,9 +22,6 @@ public interface ReferenceLinkRepo extends JpaRepository<ReferenceLink, String> 
       @Param("externalReference") String externalReference);
 
   public List<ReferenceLink> findAllByFolioReference(@Param("folioReference") String folioReference);
-
-  @Query("SELECT rl from ReferenceLink AS rl WHERE rl.type.id = :typeId")
-  public Stream<ReferenceLink> streamAllByTypeId(@Param("typeId") String typeId);
 
   @Transactional
   public long deleteByTypeName(@Param("typeName") String typeName);
