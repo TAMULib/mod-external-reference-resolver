7ebda2f724f7d5e251078c6485e76514cb98cbab
jsavell <jsavell@library.tamu.edu>
2019-11-20 15:58:21 -0600

    fix for dated reference to spring module core

--- src/main/java/org/folio/rest/model/ReferenceLinkType.java
+++ src/main/java/org/folio/rest/model/ReferenceLinkType.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
-import org.folio.rest.domain.model.AbstractBaseEntity;
+import org.folio.spring.domain.model.AbstractBaseEntity;
 
 @Entity
 public class ReferenceLinkType extends AbstractBaseEntity {
