18e5cc43973f692266960dbbe5dcf75f2f158af7
William Welling <wwelling@library.tamu.edu>
2019-12-04 15:23:37 -0600

    controller advice for friendly response

--- src/main/java/org/folio/rest/advice/ReferenceLinkControllerAdvice.java
+++ src/main/java/org/folio/rest/advice/ReferenceLinkControllerAdvice.java
@@ -2,19 +2,33 @@ package org.folio.rest.advice;
 
 import org.folio.rest.exception.ReferenceLinkExistsException;
 import org.folio.rest.model.ReferenceLink;
+import org.folio.spring.model.response.Errors;
+import org.folio.spring.utility.ErrorUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @ControllerAdvice
 public class ReferenceLinkControllerAdvice {
 
+  private static final Logger logger = LoggerFactory.getLogger(ReferenceLinkControllerAdvice.class);
+
   @ExceptionHandler(value = ReferenceLinkExistsException.class)
-  public @ResponseBody ResponseEntity<ReferenceLink> handleInvalidValuePathException(ReferenceLinkExistsException exception) {
+  public ResponseEntity<ReferenceLink> handleInvalidValuePathException(ReferenceLinkExistsException exception) {
     return ResponseEntity.accepted().body(exception.getReferenceLink());
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(ClassNotFoundException.class)
+  public Errors handleClassNotFoundException(ClassNotFoundException exception) {
+    logger.debug(exception.getMessage(), exception);
+    return ErrorUtility.buildError(exception, HttpStatus.BAD_REQUEST);
   }
 
 }
